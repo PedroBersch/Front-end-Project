c3e0176a8ea707e4374ae0a8d0022ac960f76443
Pedro Bersch <pedroleottebersch@gmail.com>
2023-11-15 22:51:41 -0300

    Movie information in html

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
+const movieTitleElement = document.querySelector('.movie-title');
+const moviePosterElement = document.querySelector('#movie-poster');
+const movieScoreElement = document.querySelector('.movie-score');
+const moviePlotElement = document.querySelector('.movie-plot');
+const movieReleaseDataElement = document.querySelector('.movie-release-data');
+const movieActorsElement = document.querySelector('.movie-actors');
+
 const apiKey = "76705089";
 const url = `http://www.omdbapi.com/?apikey=${apiKey}`;
 
 async function getMovieByTitle(title){
-    const response = await fetch(`${url}&t=${title}`)
-    const data = response.json();
-    console.log(data)
+    const response = await fetch(`${url}&t=${title}`);
+    const data = await response.json();
+    return data;
 }
-getMovieByTitle('Matrix')
+async function showMovie(title) {
+    try {
+        const movie = await getMovieByTitle(title);
+        console.log(movie);
+        
+        movieTitleElement.textContent = movie.Title;
+        moviePosterElement.src = movie.Poster;
+        movieScoreElement.textContent = `IMDb Rating: ${movie.imdbRating}`;
+        moviePlotElement.textContent = movie.Plot;
+        movieReleaseDataElement.textContent = `Released: ${movie.Released}`;
+        movieActorsElement.textContent = `Actors: ${movie.Actors}`;
+    } catch (error) {
+        console.error("Error fetching movie:", error);
+    }
+}
+showMovie("The matrix")
